0e7adf1659c474755217cd0f14a65a98904120fa
Anna van der Vet <annavdvet@hotmail.com>
2024-12-07 21:53:58 +0100

    changed the port

--- server.js
+++ server.js
@@ -299,7 +299,7 @@ Context: ${JSON.stringify(context)}`;
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on ${PORT}`);
 });
 
 process.on('unhandledRejection', (error) => {
